9a2c99e661e79b131f3fc0d8fd912d73858d79fd
granddaifuku <>
2020-06-22 19:06:28 +0900

    Added new test

---
+++ test/aoj/DSL_1_A.test.cpp
@@ -0,0 +1,24 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_1_A&lang=en"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#include "../../DataStructure/disjoint_set.cpp"
+
+int main() {
+    int n, q;
+    cin >> n >> q;
+    DisjointSet dj = DisjointSet(n);
+    for (int i = 0; i < q; ++i) {
+        int c, x, y;
+        cin >> c >> x >> y;
+        if (c) {
+            cout << (dj.isSame(x, y) ? 1 : 0) << endl;
+        } else {
+            dj.makeSet(x, y);
+        }
+    }
+
+    return 0;
+}
--- test/aoj/DSL_2_A.test.cpp
+++ test/aoj/DSL_2_A.test.cpp
@@ -1,23 +1,23 @@
-#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_1_A&lang=en"
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_A"
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-#include "../../DataStructure/disjoint_set.cpp"
+#include "../../DataStructure/segment_tree.cpp"
+
+const int Inf = 2147483647;
 
 int main() {
     int n, q;
     cin >> n >> q;
-    DisjointSet dj = DisjointSet(n);
+    SegmentTree<int> seg = SegmentTree<int>(n, Inf,
+        [](int a, int b) { return min(a, b); }, [](int a, int b) { return b; });
     for (int i = 0; i < q; ++i) {
         int c, x, y;
         cin >> c >> x >> y;
-        if (c) {
-            cout << (dj.isSame(x, y) ? 1 : 0) << endl;
-        } else {
-            dj.makeSet(x, y);
-        }
+        if (c) cout << seg.query(x, y) << endl;
+        else seg.update(x, y);
     }
 
     return 0;
