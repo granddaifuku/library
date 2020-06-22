6b6f5c7cfce4fa2403bfb1ab7cb75f9698fa88f4
granddaifuku <>
2020-06-22 15:28:27 +0900

    Fixed

--- test/aoj/DSL_2_A.cpp
+++ test/aoj/DSL_2_A.cpp
@@ -6,12 +6,12 @@ using namespace std;
 
 #include "../../DataStructure/segment_tree.cpp"
 
-const int Inf = 2147483647;
+int INF = 2147483647;
 
 int main() {
     int n, q;
     cin >> n >> q;
-    SegmentTree<int> seg = SegmentTree<int>(n, Inf,
+    SegmentTree<int> seg = SegmentTree<int>(n, INF,
         [](int a, int b) { return min(a, b); }, [](int a, int b) { return b; });
     for (int i = 0; i < q; ++i) {
         int c, x, y;
