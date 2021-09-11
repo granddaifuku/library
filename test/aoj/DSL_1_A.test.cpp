14d480e6c6cfd44bd5f26d1cb15522d4341ef2ad
granddaifuku <daifukuformac@gmail.com>
2021-09-11 17:04:03 +0900

    format

--- test/aoj/DSL_1_A.test.cpp
+++ test/aoj/DSL_1_A.test.cpp
@@ -1,4 +1,5 @@
-#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_1_A&lang=en"
+#define PROBLEM \
+  "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_1_A&lang=en"
 
 #include <bits/stdc++.h>
 
@@ -7,18 +8,18 @@ using namespace std;
 #include "../../DataStructure/disjoint_set.cpp"
 
 int main() {
-    int n, q;
-    cin >> n >> q;
-    DisjointSet dj = DisjointSet(n);
-    for (int i = 0; i < q; ++i) {
-        int c, x, y;
-        cin >> c >> x >> y;
-        if (c) {
-            cout << (dj.isSame(x, y) ? 1 : 0) << endl;
-        } else {
-            dj.makeSet(x, y);
-        }
+  int n, q;
+  cin >> n >> q;
+  DisjointSet dj = DisjointSet(n);
+  for (int i = 0; i < q; ++i) {
+    int c, x, y;
+    cin >> c >> x >> y;
+    if (c) {
+      cout << (dj.isSame(x, y) ? 1 : 0) << endl;
+    } else {
+      dj.makeSet(x, y);
     }
+  }
 
-    return 0;
+  return 0;
 }
